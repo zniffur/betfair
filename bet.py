24d1e561d51230c1993827dabab4bbba7abbcd9e
Simone Ruffino <simoneruffino@gmail.com>
2015-08-28 00:57:26 +0200

    first commit

---
+++ bet.py
@@ -0,0 +1,75 @@
+'''
+Created on 26/ago/2015
+
+@author: crisimo
+'''
+import utils
+
+username='sruffinoit'
+password='na8a880minkia'
+
+if __name__ == '__main__':
+    
+    token = utils.bf_login(username=username, password=password)
+    
+    # status = utils.bf_keepalive(token)
+    
+    # status = utils.bf_logout(token)
+        
+    # all events types
+    status = utils.bf_do('listEventTypes', token=token)
+    
+    #list competitions- soccer only
+    status = utils.bf_do('listCompetitions', token=token, params={"filter": {"eventTypeIds": ["1"]}})
+    
+    #list competitions which have live events
+    status = utils.bf_do('listCompetitions', token=token, params={"filter": {"inPlayOnly": True}})
+
+    #list competitions which have live events and are Soccer
+    status = utils.bf_do('listCompetitions', token=token, params={"filter": {"inPlayOnly": True, "eventTypeIds": ["1"]}})
+    
+    #list live events, soccer
+    status = utils.bf_do('listEvents', token=token, params={"filter":{"inPlayOnly": True, "eventTypeIds": ["1"]}})
+    
+    #list live events, UEFA CL
+    status = utils.bf_do('listEvents', token=token, params={"filter":{"inPlayOnly": True, "competitionIds": ["228"]}})
+    
+    # filters one live event (e.g. UEFA CL soccer game)
+    status = utils.bf_do('listEvents', token=token, params={"filter":{"inPlayOnly": True, "eventIds": ["27515964"]}})
+    
+    # list markets associated w/ one event, e.g. MATCH_ODDS, OVER_UNDER_15 on a UEFA CL game
+    status = utils.bf_do('listMarketTypes', token=token, params={"filter":{"inPlayOnly": True, "eventIds": ["27515964"]}})
+    
+    # markets details for one event  ('listMarketTypes')
+    status = utils.bf_list_mkt(token, '27516487')
+    status = utils.bf_list_mkt(token, '27516487', text_query='match*')
+    
+    # get market prices for one market (listMarketBook)
+    status = utils.bf_get_mkt_prices(token, '1.120158817')
+    
+    #place bet on one specific result (selection, e.g. draw, 0-0, U1.5)
+    mkt_id = '1.120158817'
+    selection_id = '58805'
+    side = 'LAY'
+    size = '2'  # $
+    price = '1.67' # @this price
+    status = utils.bf_place_bet(token, mkt_id, selection_id, side, size, price)
+    
+    # check orders on one mkt
+    status = utils.bf_do('listCurrentOrders', token=token, params={"marketIds":["1.120158817"],"orderProjection":"ALL","dateRange":{}})
+    
+    # check closed (settled) bets
+    status = utils.bf_do('listClearedOrders', token=token, params={"betStatus":"SETTLED"})
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ utils.py
@@ -0,0 +1,123 @@
+import requests
+import json
+
+APP_NAME = 'SRBetfairApp'
+APP_KEY = 'AYW5U5tNdmrLsvA4'
+APP_KEY_DELAY = 'd1vcqiZ6Woq08sfC'
+
+#LOGIN
+def bf_login(username='sruffinoit', password='na8a880minkia'):
+    
+    header = { 'X-Application' : APP_KEY, 'Accept' : 'application/json', 'Content-Type': 'application/x-www-form-urlencoded' }
+
+    up = 'username=' + username + '&password=' + password
+    url = 'https://identitysso.betfair.it/api/login'
+    response = requests.post(url, data=up, headers=header)
+    print json.dumps(json.loads(response.text), indent=3)
+    
+    return response.json()['token']
+
+# KEEPALIVE
+def bf_keepalive(token):
+    url = 'https://identitysso.betfair.it/api/keepAlive'
+    header = { 'X-Application' : APP_KEY, 'X-Authentication' : token,'Accept' : 'application/json' }
+    response = requests.post(url, headers=header)
+    print json.dumps(json.loads(response.text), indent=3)    
+    return response.status_code
+
+# LOGOUT
+def bf_logout(token):
+    url = 'https://identitysso.betfair.it/api/logout'
+    header = { 'X-Application' : APP_KEY, 'X-Authentication' : token,'Accept' : 'application/json' }
+    response = requests.post(url, headers=header)
+    print json.dumps(json.loads(response.text), indent=3)    
+    return response.status_code
+
+#DO_SMTHING
+def bf_do(api, token, params={"filter":{ }}):
+    endpoint = "https://api.betfair.com/exchange/betting/rest/v1.0/"
+    header = { 'X-Application' : APP_KEY, 'X-Authentication' : token ,'content-type' : 'application/json' }
+    url = endpoint + api + "/"
+    response = requests.post(url, data=json.dumps(params), headers=header)
+    print json.dumps(json.loads(response.text), indent=3)
+    return response.status_code
+
+def bf_list_mkt(token, event_id, text_query=None):
+    if text_query is not None:
+        params = {
+                "filter": {
+                           #"eventTypeIds": ["2"],
+                           #"inPlayOnly": True,
+                           "textQuery": text_query,
+                           "eventIds": [event_id]
+                },
+                "maxResults": "200",
+                "marketProjection": [
+                    "COMPETITION",
+                    "EVENT",
+                    "EVENT_TYPE",
+                    "RUNNER_DESCRIPTION",
+                    "RUNNER_METADATA",
+                    "MARKET_START_TIME"
+                    ]
+                  }
+    else:
+        params = {
+                "filter": {
+                           #"eventTypeIds": ["2"],
+                           #"inPlayOnly": True,
+                           "eventIds": [event_id]
+                },
+                "maxResults": "200",
+                "marketProjection": [
+                    "COMPETITION",
+                    "EVENT",
+                    "EVENT_TYPE",
+                    "RUNNER_DESCRIPTION",
+                    "RUNNER_METADATA",
+                    "MARKET_START_TIME"
+                    ]
+                  }        
+    return bf_do('listMarketCatalogue', token=token, params=params)
+    
+def bf_get_mkt_prices(token, mkt_id):
+    params = {
+              "marketIds": [mkt_id],
+              #"orderProjection": "EXECUTABLE",
+              "priceProjection": {
+                            #"priceData" : ["EX_ALL_OFFERS"],
+                            "priceData" : ["EX_BEST_OFFERS"],
+                            "exBestOffersOverrides": {
+                                                      "bestPricesDepth": 3
+                                                      }
+                            }
+              }
+    return bf_do('listMarketBook', token=token, params=params)
+
+
+def bf_place_bet(token, mkt_id, selection_id, side, size, price):
+    '''
+    param:selection_id   id del risultato puntato
+    param:side "BACK" (punta) "LAY" (banca)
+    param:size e.g. "2" euro
+    '''
+    params = {
+            "marketId": mkt_id,
+            "instructions": [
+                {
+                    "selectionId": selection_id,
+                    "handicap": "0",
+                    "side": side,
+                    "orderType": "LIMIT",
+                    "limitOrder": {
+                        "size": size,
+                        "price": price,
+                        "persistenceType": "LAPSE"
+                    }
+                }
+            ]
+        }
+    return bf_do('placeOrders', token=token, params=params)
+    
+
+
